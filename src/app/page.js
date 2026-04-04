61d74b3c9e74bc080dcab841d2ca8cda84cb5f5a
Nsimba Afonso <nsimbaafonso0@gmail.com>
2026-04-04 18:45:29 +0100

    Seções removidas da home

--- src/app/page.js
+++ src/app/page.js
@@ -1,9 +1,7 @@
 import Hero from "@/components/home/Hero";
-import Sobre from "@/components/home/Sobre";
 import Servicos from "@/components/home/Servicos";
 import Pacotes from "@/components/home/Pacotes";
 import Portfolio from "@/components/home/Portfolio";
-import Parallax from "@/components/home/Parallax";
 import Faq from "@/components/home/Faq";
 import Contato from "@/components/home/Contato";
 
@@ -16,11 +14,9 @@ export default function Home() {
   return (
     <>
       <Hero />
-      <Sobre />
       <Servicos />
       <Pacotes />
       <Portfolio />
-      <Parallax />
       <Faq />
       <Contato />
     </>
